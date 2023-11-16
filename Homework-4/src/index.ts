b6c6a5ad5df06e0cf42e7cc835a9f54d7cbbdf69
Viktor Shmatko <37986452+Viktor727@users.noreply.github.com>
2023-11-16 17:06:07 +0200

    homework 4 -updated

--- Homework-4/src/index.ts
+++ Homework-4/src/index.ts
@@ -1,42 +1,61 @@
-enum colors {
-  blue = "blue",
-  red = "red",
-  green = "green",
-  yellow = "yellow",
-  black = "black",
-  white = "white",
-  gray = "gray",
-  purple = "purple",
-  orange = "orange",
-  pink = "pink"
+// 1.✅ Хорошою практикою вважається писати назву enum та пропертів в ньому з великої літери 
+// 2.✅ Реалізація класів повинна базуватися на інтерфейсах, існує навіть такий підхід, коли ми спочатку створюємо абстракції, 
+// а потім відповідно до цих абстракцій пишемо наші класи, тобто тут я б виділив інтерфейс IShape і заімплементив його до класу Shape
+
+// 3.✅ Методи getName і getColor (Доречі, зазвичай ім'я методів мають бути дієсловами, адже це дія, тобто getColor, getShape, calculateSomething і тд, 
+// інакше інші розробники можуть подумати, що це звичайні поля) виглядають мені зайвими, адже просто можна прибрати protected, та стукатись до полів напряму 
+// ззовні. Або зробити поля name та color приватними і додати гетери до них.
+
+// 4.✅ calculateArea не повинен бути гетером. Гетери використовуються для того, щоб просто повернути значення якогось поля (тому назви гетерів завжди іменники),
+// а от якщо потрібно виконати якісь операції, наразі це розрахувати площу і повернути її значення, то calculateArea має бути звичайним методом і 
+// сама назва нам про це говорить (calculateArea - якась дія, дієслово)
+// 5.✅ Якщо є якась додаткова логіка (поля або методи), яка має слідкувати певним правилам і може бути присутня в різних класах, вона також має бути 
+// описана в окремому інтерфейсі. Наприклад print можна описати у інтерфейсі IPrintable, де зазначити, що класи, які імплементують цей інтерфейс повинні 
+// мати цей метод, який повинен повертати void. Бо наразі кожен клас може описувати цей метод по своєму, десь ми можемо повернути строку, десь число, 
+// десь не повернути нічого і помилки не буде
+// 6. ✅ Число PI можна отримати з  Math.PI, так ми будемо мати максимально точне значення і позбудемося зайвого поля.
+// 7. ✅ Triangle зараз також має пропертю Pi, хоча вона йому не потрібна)
+// 8. ✅ В назвах пропертів і методів раджу уникати таких назв як side1, side2 і давати більш конкретні назви. наприклад width та height. 
+// Square наприклад взагалі не потрібен розмір двох сторін для розрахунку площі, адже його сторони рівні
+
+enum Colors {
+  Blue = "blue",
+  Red = "red",
+  Green = "green",
+  Yellow = "yellow",
+  Black = "black",
+  White = "white",
+  Gray = "gray",
+  Purple = "purple",
+  Orange = "orange",
+  Pink = "pink",
 }
 
-
-abstract class Shape {
-  constructor(protected readonly name: string, protected readonly color: colors) {}
-
-  abstract get calculateArea(): number;
-
-  shapeName(): string{
-      return this.name;
-  }
-
-  shapeColor(): string{
-      return this.color;
-  }
+interface IShape {
+  calculateArea(): number;
 }
 
-class Rectangle extends Shape {
+interface IPrintable {
+  print(): void;
+}
+
+abstract class Shape implements IShape {
+  constructor(readonly name: string, readonly color: Colors) {}
+
+  abstract calculateArea(): number;
+}
+
+class Rectangle extends Shape implements IPrintable {
   constructor(
     readonly name: string,
-    readonly color: colors,
+    readonly color: Colors,
     private length: number,
     private width: number
   ) {
     super(name, color);
   }
 
-  get calculateArea(): number {
+  calculateArea(): number {
     return this.length * this.width;
   }
 
@@ -45,54 +64,49 @@ class Rectangle extends Shape {
   }
 }
 
-class Square extends Shape {
+class Square extends Shape implements IPrintable {
   constructor(
     readonly name: string,
-    readonly color: colors,
-    private side1: number,
-    private side2: number
+    readonly color: Colors,
+    private widthOrHeight: number,
   ) {
     super(name, color);
   }
 
-  get calculateArea(): number {
-    return this.side1 * this.side2;
+  calculateArea(): number {
+    return this.widthOrHeight * this.widthOrHeight;
   }
 
-  print(): void{
-    console.log("Formula: Area = side * side")
+  print(): void {
+    console.log("Formula: Area = width * height");
   }
 }
 
 class Circle extends Shape {
-  readonly pi: number = 3.14;
-
   constructor(
     readonly name: string,
-    readonly color: colors,
+    readonly color: Colors,
     private radius: number
   ) {
     super(name, color);
   }
 
-  get calculateArea(): number {
-    return (this.radius * this.radius) * this.pi;
+  calculateArea(): number {
+    return (this.radius * this.radius) * Math.PI;
   }
 }
 
 class Triangle extends Shape {
-  readonly pi: number = 3.14;
-
   constructor(
     readonly name: string,
-    readonly color: colors,
+    readonly color: Colors,
     private base: number,
     private height: number
   ) {
     super(name, color);
   }
 
-  get calculateArea(): number {
+  calculateArea(): number {
     return this.base * this.height * 0.5;
   }
 }
@@ -101,31 +115,31 @@ class Triangle extends Shape {
 
 
 
-const rectangle1 = new Rectangle("rectangle", colors.pink, 5, 5);
-console.log(rectangle1.shapeName());
-console.log(rectangle1.shapeColor());
-console.log(rectangle1.calculateArea);
+const rectangle1 = new Rectangle("rectangle", Colors.Pink, 5, 5);
+console.log(rectangle1.name);
+console.log(rectangle1.color);
+console.log(rectangle1.calculateArea());
 
 console.log("")
 
-const square1 = new Square("square", colors.blue, 10, 10);
-console.log(square1.shapeName());
-console.log(square1.shapeColor());
-console.log(square1.calculateArea); 
+const square1 = new Square("square", Colors.Blue, 10);
+console.log(square1.name);
+console.log(square1.color);
+console.log(square1.calculateArea()); 
 
 console.log("");
 
-const circle1 = new Circle("circle", colors.blue, 10);
-console.log(circle1.shapeName());
-console.log(circle1.shapeColor());
-console.log(circle1.calculateArea); 
+const circle1 = new Circle("circle", Colors.Blue, 10);
+console.log(circle1.name);
+console.log(circle1.color);
+console.log(circle1.calculateArea()); 
 
 console.log("");
 
-const triangle1 = new Triangle("triangle", colors.blue, 10, 5);
-console.log(triangle1.shapeName());
-console.log(triangle1.shapeColor());
-console.log(triangle1.calculateArea); 
+const triangle1 = new Triangle("triangle", Colors.Blue, 10, 5);
+console.log(triangle1.name);
+console.log(triangle1.color);
+console.log(triangle1.calculateArea()); 
 
 console.log("");
 
